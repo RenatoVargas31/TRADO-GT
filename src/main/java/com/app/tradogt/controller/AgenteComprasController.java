f206bda447a48915d27717635d5063999644683b
dodo240703 <a20202396@pucp.edu.pe>
2024-09-16 18:43:02 -0500

    Algunos controladores con htmls vacios Agente

--- src/main/java/com/app/tradogt/controller/AgenteComprasController.java
+++ src/main/java/com/app/tradogt/controller/AgenteComprasController.java
@@ -17,20 +17,38 @@ public class AgenteComprasController {
     public String changePass(){
         return "Agente/changePass-Agente";
     }
-
+    //Tableros de ordenes
     @GetMapping("/allOrders")
     public String showAllOrders() {
         return "Agente/allOrdersTable-Agente";
     }
-
+    @GetMapping("/sinAsignarOrders")
+    public String showSinAsignarOrders() {
+        return "Agente/sinAsignarOrdersTable-Agente";
+    }
+    @GetMapping("/pendingOrders")
+    public String showPendingOrders() {
+        return "Agente/pendingOrdersTable-Agente";
+    }
+    @GetMapping("/enProcesoOrders")
+    public String showEnProcesoOrders() {
+        return "Agente/procesoOrdersTable-Agente";
+    }
+    @GetMapping("/solveOrders")
+    public String showSolveOrders() {
+        return "Agente/solveOrdersTable-Agente";
+    }
     @GetMapping("/perfil")
     public String showPerfil() {
         return "Agente/profile-Agente";
     }
 
+    //PREGUNTAS FRECUENTES 
     @GetMapping("/faq")
     public String showFaq() {
         return "Agente/faq-Agente";
     }
+
+
 
 }
