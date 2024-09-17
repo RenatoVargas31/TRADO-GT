52018929428ace8812bffffa8e27e198b00b24e3
Xavier <x.gavidia@pucp.edu.pe>
2024-09-16 20:22:11 -0500

    V1 loguin

---
+++ src/main/java/com/app/tradogt/controller/SistemaController.java
@@ -0,0 +1,16 @@
+package com.app.tradogt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sistema")
+public class SistemaController {
+    @GetMapping("/loguin")
+    public String viewloguin(){return "loguin";}
+    @GetMapping("/PassRestore")
+    public String viwPassRestore(){return "ContraseñaRestore";}
+    @GetMapping("/CreateAcc")
+    public String viwCreateAcc(){return "CreateAcc";}
+}
