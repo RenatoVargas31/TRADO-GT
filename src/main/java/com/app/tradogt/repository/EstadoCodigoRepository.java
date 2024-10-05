3932692da06da94bec4a36c2c3b37db4843ddb66
RenatoVargas31 <rlvs_312001_1999@outlook.com>
2024-10-05 01:42:16 -0500

    Modificación entyties (eliminación de tablas de CodigoDespachador.java, CodigoJurisdiccion.java y EstadoCodigo.java) adición de codigo de despachador como tributo de usuario y eliminación del repositorio del estado del código

--- src/main/java/com/app/tradogt/repository/EstadoCodigoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.tradogt.repository;
-
-import com.app.tradogt.entity.EstadoCodigo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstadoCodigoRepository extends JpaRepository<EstadoCodigo, Integer> {
-}
