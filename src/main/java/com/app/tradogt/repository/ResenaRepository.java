61e97efd5bf776f73cb843da0df29fb46b03797e
Sergio <a20202137@pucp.edu.pe>
2024-10-11 16:10:49 -0500

    Ver reseñas de los usuarios

--- src/main/java/com/app/tradogt/repository/ResenaRepository.java
+++ src/main/java/com/app/tradogt/repository/ResenaRepository.java
@@ -1,7 +1,9 @@
 package com.app.tradogt.repository;
 
 
+import com.app.tradogt.entity.Producto;
 import com.app.tradogt.entity.Resena;
+import com.app.tradogt.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,10 @@ import java.util.List;
 
 @Repository
 public interface ResenaRepository extends JpaRepository<Resena, Integer> {
+    List<Resena> findAll();
+
+    // Si necesitas filtrar reseñas por producto o usuario, podrías agregar más métodos
+    List<Resena> findByUsuarioIdusuario(Usuario usuario);
+
+    List<Resena> findByProductoIdproducto(Producto producto);
 }
