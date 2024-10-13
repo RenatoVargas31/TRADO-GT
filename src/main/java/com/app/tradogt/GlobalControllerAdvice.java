904e925ab97a80bc01de60d2e98f34a9414a6f9c
RenatoVargas31 <rlvs_312001_1999@outlook.com>
2024-10-13 03:08:13 -0500

    Información de UsuarioAutenticado de forma global y nueva vista de navbar

--- src/main/java/com/app/tradogt/GlobalControllerAdvice.java
+++ src/main/java/com/app/tradogt/GlobalControllerAdvice.java
@@ -1,8 +1,11 @@
 package com.app.tradogt;
 
 import com.app.tradogt.entity.CustomUserDetails;
+import com.app.tradogt.entity.Usuario;
+import com.app.tradogt.repository.UsuarioRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.ui.Model;
@@ -10,11 +13,29 @@ import org.springframework.ui.Model;
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
+    final UsuarioRepository usuarioRepository;
+
+    public GlobalControllerAdvice(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+    
     @ModelAttribute
-    public void addUserDetailsToModel(Model model) {
+    private void getAuthenticatedUser(Model model) {
+        // Obtener el usuario autenticado
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails customUserDetails) {
-            model.addAttribute("currentUser", customUserDetails);
+        Object principal = authentication.getPrincipal();
+        String correoUsuario;
+
+        if (principal instanceof UserDetails) {
+            correoUsuario = ((UserDetails) principal).getUsername(); // Obtener el correo del usuario autenticado
+        } else {
+            correoUsuario = principal.toString();
         }
+
+        // Buscar el usuario (agente) en la base de datos usando el correo
+        Usuario usuario = usuarioRepository.findByCorreo(correoUsuario);  //Buscar usuario por correo
+        // Agregar el usuario a la vista
+        model.addAttribute("usuarioAutenticado", usuario);
+
     }
 }
